1fa878d11d9ebc0144e2dd5cfc725653ee55aa7a
Akira Hayatake <ggmg789@gmail.com>
2015-01-20 07:50:58 +0900

    bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 SET project_path=%~dp0
-SET atom_shell_path=..\atom-shell-v0.20.2-win32-ia32
+cd ../
+SET atom_shell_path=%cd%\atom-shell-v0.20.2-win32-ia32
 cd %project_path%
 %atom_shell_path%\atom.exe %project_path%
